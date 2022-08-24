57105391ef30fc4ab0b23d32d78acb208c4fb795
Nicolás Montenegro <niko_nmv@live.cl>
2022-08-24 19:57:21 -0400

    Added Pixeledleded font

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/sass/export.sass'],
+  css: [
+    '~/assets/sass/export.sass',
+    '~/assets/sass/fonts.sass'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
